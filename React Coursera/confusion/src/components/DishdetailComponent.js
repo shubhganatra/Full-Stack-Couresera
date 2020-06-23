22537b327df7df20de01ce20c6b4382f4577df17
shubhganatra <shubhv611@gmail.com>
2020-06-23 10:55:23 +0530

    Single Page Applications Part 2

--- React Coursera/confusion/src/components/DishdetailComponent.js
+++ React Coursera/confusion/src/components/DishdetailComponent.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
-
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom'
 
     function RenderComments({comments})
     {
@@ -69,12 +69,22 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 're
         
         return(
             <div className='container'>
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to='/home'>Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active><Link to={`/menu/${props.dish.id}`}>{props.dish.name}</Link></BreadcrumbItem>
+                     </Breadcrumb>
+                    <div className='col-12'>
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>              
+                </div>
                 <div className='row'>
                     <RenderDish dish={props.dish} />
-                    <RenderComments comments={props.dish.comments} />
+                    <RenderComments comments={props.comments} />
                 </div>
             </div>
-            
         );
     }
 
--- React Coursera/confusion/src/components/MenuComponent.js
+++ React Coursera/confusion/src/components/MenuComponent.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom'
 
-function RenderMenuItem({ dish, onClick}) {
+function RenderMenuItem({ dish }) {
     return (
         <div>
-            <Card onClick={() => onClick(dish.id)}>
-                <CardImg width="100%" src={dish.image} alt={dish.name} />
-                <CardImgOverlay>
-                    <CardTitle>{dish.name}</CardTitle>
-                </CardImgOverlay>
+            <Card>
+                <Link to={`/menu/${dish.id}`} >
+                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardImgOverlay>
+                        <CardTitle>{dish.name}</CardTitle>
+                    </CardImgOverlay>
+                </Link>
             </Card>
         </div>
     )
@@ -18,16 +21,24 @@ const Menu = (props) => {
     const menu = props.dishes.map((dish) => {
             return (
                 <div key={dish.id} className="col-12 col-md-5 m-1">
-                    <RenderMenuItem dish={dish} onClick={props.onClick} />
+                    <RenderMenuItem dish={dish}/>
                 </div>
             );
         });
         return(
             <div className="container">
                 <div className="row">
-                    {menu}
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to='/home'>Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active><Link to='/menu'>Menu</Link></BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className='col-12'>
+                        <h3>Menu</h3>
+                        <hr />
+                    </div>
                 </div>
-                <div className="row">                 
+                <div className="row">
+                    {menu}
                 </div>
             </div>
         );
